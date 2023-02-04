a8933649ec0de4dfc7d88d04b36f20994db223c7
Erick Gonzalez <deweyerick@gmail.com>
2023-02-03 20:01:46 -0600

    responsive deck animation

--- Source/DeckAnimation.cpp
+++ Source/DeckAnimation.cpp
@@ -24,21 +24,23 @@ void DeckAnimation::update ()
 }
 void DeckAnimation::paint (Graphics& g)
 {
+    float responsiveSize = (float) getLocalBounds().getWidth()*0.009f;
+     
     g.fillAll   (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
     
     // bigger circle
-    int d1 = 100;
+    int d1 = 100 * responsiveSize;
     g.setColour(Colours::black);
     g.fillEllipse(getWidth()/2 - d1/2, getHeight()/2 -d1/2, d1, d1);
     
     // small circle
-    int d2 = 40;
+    int d2 = 40 * responsiveSize ;
     g.setColour(Colours::lightblue);
     g.fillEllipse(getWidth()/2 - d2/2, getHeight()/2 -d2/2, d2, d2);
     
     // deck line animation
-    int pointRadius1 = 50;
-    int pointRadius2 = 20;
+    int pointRadius1 = 50 * responsiveSize;
+    int pointRadius2 = 20 * responsiveSize;
     
     Point<float> startPoint (animateX(pointRadius1 , 0), animateY(pointRadius1 , 0));
     Point<float> endPoint   (animateX(pointRadius2 , 0), animateY(pointRadius2 , 0));
@@ -48,8 +50,8 @@ void DeckAnimation::paint (Graphics& g)
      
     // ring deck animation
     
-    int ringRadius = 50;
-    auto numberOfDots = 62;
+    int ringRadius = 50 * responsiveSize;
+    auto numberOfDots = 62 ;
     Path path;
 
     // i = 2 to offset the start of the path
